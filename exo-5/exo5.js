b289254b3a8797dc8af12f008b568ae4efa7b42c
hyejin-marichal <hyejin-marichal@users.noreply.github.com>
2020-03-27 17:19:38 +0100

    exo- 5

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,17 @@
 // you can write js here
 
 console.log('exo-5');
+
+let input = 'Lorem ipsum dolor sit amet.';
+let vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+let resultArray = [];
+for (let i = 0; i < input.length; i++) {
+    // console.log(input.charAt(i));
+    // console.log(vowels.indexOf(input.charAt(i)));
+    for (let j = 0; j < vowels.length; j++) {
+        if (input.charAt(i) === vowels[j]) {
+            let addresultArray = resultArray.push(input.charAt(i));
+        }
+    }
+}
+console.log(resultArray.join('').toUpperCase());
