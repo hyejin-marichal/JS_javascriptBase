80e6b72af730412062b04d6017e930d083662531
hyejin-marichal <hyejin-marichal@users.noreply.github.com>
2020-03-27 17:18:46 +0100

    exo- 4

--- exo-4/script.js
+++ exo-4/script.js
@@ -3,3 +3,39 @@
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+
+// 1. enlever le dernier éléments du tableau secretMessage.
+let removeLast = secretMessage.pop();
+// console.log(secretMessage);
+console.log(secretMessage.join(' '));
+
+// 2.ajouter les mots 'to' and 'program' comme des éléments distincts à la fin du tableau.
+let addWords = secretMessage.push('to','programme');
+// console.log(secretMessage);
+console.log(secretMessage.join(' '));
+
+// 3. Changer le mot 'easily' par 'right' en accédant au bon index du tableau
+let index = secretMessage.indexOf('easily');
+let changeWord = secretMessage.splice(index,1,'right');
+// console.log(secretMessage);
+console.log(secretMessage.join(' '));
+
+// 4.supprimer le premier éléments du tableau
+let removeFirst = secretMessage.shift();
+// console.log(secretMessage);
+console.log(secretMessage.join(' '));
+
+// 5. ajouter la chaine "Programming" en début de tableau
+let addFirst = secretMessage.unshift('Programming');
+// console.log(secretMessage);
+console.log(secretMessage.join(' '));
+
+//6. remplacer les mots 'get', 'right', 'the', 'first', 'time' par 'know'
+let postionStart = secretMessage.indexOf('get');
+let changeWords =secretMessage.splice(postionStart,5,'know');
+// console.log(secretMessage);
+console.log(secretMessage.join(' '));
+
+// 7. la méthode .join pour afficher le message secret à la console
+// console.log(secretMessage);
+console.log(secretMessage.join(' '));
