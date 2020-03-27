f4775a3d97ecb1fe33f85e84609b654cc3d1f737
hyejin-marichal <hyejin-marichal@users.noreply.github.com>
2020-03-27 17:18:22 +0100

    exo- 2

--- exo-2/script.js
+++ exo-2/script.js
@@ -6,16 +6,18 @@ var raceNumber = Math.floor(Math.random() * 1000);
 
 if (!isInscrit) {
     raceNumber += 1000;
+} else {
+    raceNumber += 1;
 }
 
-if (age >= 18 && isInscrit) {
+if (age > 18 && isInscrit) {
     console.log('You will race at 9:30 am');
-} else if (age >= 18 || isInscrit) {
+} else if (age > 18 || isInscrit) {
     console.log('You will race at 11:00 am ' + raceNumber);
 } else if (age < 18 || !isInscrit) {
     console.log('You will race at 12:30 pm ' + raceNumber);
 } else {
-    console.log('Go see the registration desk'); // a qoi ca sert ce else?
+    console.log('Go see the registration desk'); // a quoi ca sert cet else?
 }
 
 
