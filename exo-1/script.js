b58d3a9871f247fcf772d337997e7031f817bd2a
hyejin-marichal <hyejin-marichal@users.noreply.github.com>
2020-03-27 17:19:29 +0100

    exo- 1

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,9 +1,12 @@
 // you can write js here
-var kelvin = prompt("Quelle est la température en Kelvin aujourd'hui ?");
+
+// let kelvin = 294;
+let kelvin = prompt("Quelle est la température en Kelvin aujourd'hui ?");
 let celsius = kelvin - 273; //  La température en Celsius est de 273 degrés inférieure à celle en Kelvin.
 let fahrenheit = Math.floor(celsius * (9/5) + 32); // une température en Fahrenheit avec la formule : Fahrenheit = Celsius * (9/5) + 32
                                                         //Math.floor(number) :modifier le résultat de la variable fahrenheit pour obtenir un entier
 
 console.log('hello from file');
+console.log(kelvin);
 console.log(celsius);
 console.log(fahrenheit);
